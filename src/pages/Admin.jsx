87eafe958a3b0f1d16d04611524b0917b96bc27c
Hirenchhaniyara <purasa.corporation@gmail.com>
2025-07-08 13:37:00 +0530

    Add files via upload

---
+++ src/pages/Admin.jsx
@@ -0,0 +1,7 @@
+export default function Admin() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Admin Dashboard</h1>
+    </div>
+  );
+}
---
+++ src/pages/Staff.jsx
@@ -0,0 +1,7 @@
+export default function Staff() {
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <h1 className="text-3xl font-bold">Staff Dashboard</h1>
+    </div>
+  );
+}
